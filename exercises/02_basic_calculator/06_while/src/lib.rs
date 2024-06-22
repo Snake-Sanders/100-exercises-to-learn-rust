7de2e52cb1816bc5cff2017fe1b281af45efed37
Snake Sanders <elmarian22@gmail.com>
2024-06-22 09:37:54 +0200

    02/08

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,18 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut i = n;
+    let mut r = n;
+    if n <= 1 {
+        return 1;
+    } else {
+        while (i > 1) {
+            i -= 1;
+            r = r * i;
+        }
+
+        return r;
+    }
 }
 
 #[cfg(test)]
